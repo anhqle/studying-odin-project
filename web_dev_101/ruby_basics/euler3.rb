f78f6b162b439051bd3bbe435ea16c15abf3c876
LaDilettante <aql3@duke.edu>
2014-04-28 03:13:45 -0400

    test_first_ruby done up to 03 tests

--- web_dev_101/ruby_basics/euler3.rb
+++ web_dev_101/ruby_basics/euler3.rb
@@ -1,18 +1,20 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 
 # What is the largest prime factor of the number 600851475143 ?
-num = 1032
+num = 13195
 
-res = Array.new
-primes = Array.new
-i = 1
+res = []
+primes = []
+i = 2
 while i < num
     i_is_prime = true
     primes.each do |prime|
         i_is_prime = (i % prime == 0) ? false : true
+        break if i_is_prime == false
     end
 
     if i_is_prime
+        primes = primes + [i]
         res = (num % i == 0) ? res + [i] : res
     end
 
