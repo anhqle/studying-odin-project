0fa73cc105bef95be61c00689ca5557f7f8ff232
Anh Le <aql3@duke.edu>
2014-04-29 20:15:08 -0400

    project_rails form-based workflow feature completed

---
+++ web_dev_101/project_rails/blogger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = 'ee2a90121e6eadf25ebe148c382f45fca61bbddebd1c68059aa3c7d843b6e3af69bc4bdeff6c7a0e6a8e73d1870054e69c56ca06ab7b8009b41dec27602cec99'
---
+++ web_dev_101/project_rails/blogger/config/routes.rb
@@ -0,0 +1,4 @@
+Blogger::Application.routes.draw do
+  root to: 'articles#index'
+  resources :articles
+end
